7132eb706231d0d0549894518763d86ea2129998
borec <borec>
2026-03-24 08:44:21 +0100

    second commit

--- src/TCPserver.java
+++ src/TCPserver.java
@@ -1,3 +1,4 @@
+import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.Socket;
 import java.util.Scanner;
@@ -12,7 +13,33 @@ public class TCPserver implements Runnable {
     @Override
     public void run() {
         System.out.println("Připojil se klient");
-        Scanner dataKlienta = new Scanner(socketOdKlienta.getInputStream());
-        PrintWriter dataPosilanaTCPklientovi
+
+        try (
+                Scanner dataKlienta = new Scanner(socketOdKlienta.getInputStream());
+                PrintWriter dataPosilanaTCPklientovi = new PrintWriter(socketOdKlienta.getOutputStream(), true)
+        ) {
+            // pošleme zprávu klientovi
+            dataPosilanaTCPklientovi.println("Připojení navázáno");
+
+            // čteme data od klienta
+            while (dataKlienta.hasNextLine()) {
+                String zprava = dataKlienta.nextLine();
+                System.out.println("Klient: " + zprava);
+
+                // echo zpět klientovi
+                dataPosilanaTCPklientovi.println("Server přijal: " + zprava);
+            }
+
+        } catch (IOException e) {
+            System.out.println("Chyba komunikace: " + e.getMessage());
+        } finally {
+            try {
+                socketOdKlienta.close();
+            } catch (IOException e) {
+                System.out.println("Chyba při zavírání socketu");
+            }
+        }
+
+        System.out.println("Klient se odpojil..");
     }
 }
